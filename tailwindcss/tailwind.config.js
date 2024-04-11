f8b1f97b360eaf36f4a78b172b0d499e653f4ba4
Aman Rai <85382688+KamekaziAman@users.noreply.github.com>
2024-04-11 12:34:50 +0530

    Add files via upload

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./dict/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nff: ['"Patrick Hand", cursive;']
+      },
+    }
+  },
+}
